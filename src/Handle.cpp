ed355a2b69731ca0b3bb18ce6c39353e5c2e066c
MatsueSS <newmailnoways199@gmail.com>
2025-02-08 07:38:32 +0300

    Create definition of Handle.h

---
+++ src/Handle.cpp
@@ -0,0 +1,24 @@
+#include "Handle.h"
+#include "Log.h"
+
+#include <unistd.h>
+
+bool Handle::read_fd(int sockfd)
+{
+    if((n = read(sockfd, buf, MAXLINE-1)) > 0){
+        if(buf[n-1] == '\n')
+            buf[n-1] = 0;
+        else
+            buf[n] = 0;
+        return 1;
+    }
+    if(errno == ECONNRESET){
+        Log::make_note("1002");
+    }
+    return 0;
+}
+
+void Handle::write_str(const std::string& str, int sockfd)
+{
+    write(sockfd, str.c_str(), str.size());
+}
