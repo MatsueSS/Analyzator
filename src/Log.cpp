b095f7b55833a729408bbdbb26c414e9a36cd5bd
MatsueSS <newmailnoways199@gmail.com>
2025-02-08 05:03:49 +0300

    Create new definition Log.cpp and edit Log.h. Add comment.

---
+++ src/Log.cpp
@@ -0,0 +1,33 @@
+#include "Log.h"
+
+#include <chrono>
+#include <fstream>
+
+int Log::que_size = 0;
+std::mutex Log::mtx;
+std::queue<std::string> Log::que;
+
+void Log::flush_file()
+{
+    std::lock_guard<std::mutex> lg(mtx);
+    std::ofstream file("../res/log.bin", std::ios_base::app | std::ios_base::binary);
+    while(!que.empty()){
+        file.write(que.front().c_str(), que.front().size());
+        que.pop();
+    }
+    file.write("\0", 1);
+    file.flush();
+    file.close();
+    que_size = 0;
+}
+
+void Log::make_note(const std::string& str)
+{
+    //In this place probably race, but this doesn't matter
+    if(que_size > MAX_BUFFER_QUEUE)
+        flush_file();
+    std::lock_guard<std::mutex> lg(mtx);
+    que_size++;
+    que.push(std::to_string(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()))
+    + ' ' + str + '\n');
+}
