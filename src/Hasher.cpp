908016efaacbe519ba8737448d3997475ca5fc2c
MatsueSS <newmailnoways199@gmail.com>
2025-02-08 06:19:44 +0300

    Create file-definition of Hasher.h

---
+++ src/Hasher.cpp
@@ -0,0 +1,22 @@
+#include "Hasher.h"
+
+#include <iostream>
+
+std::string Hasher::make_hash(const std::string &psw)
+{
+    std::string hash(crypto_pwhash_STRBYTES, '\0');
+    if(crypto_pwhash_str(
+        hash.data(),
+        psw.c_str(),
+        psw.length(),
+        crypto_pwhash_OPSLIMIT_INTERACTIVE,
+        crypto_pwhash_MEMLIMIT_INTERACTIVE
+    ) != 0);
+        return "";
+    return hash;
+}
+
+bool Hasher::verify_password(const std::string &hashed, const std::string &psw)
+{
+    return crypto_pwhash_str_verify(hashed.c_str(), psw.c_str(), psw.length()) == 0;
+}
