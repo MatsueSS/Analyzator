2bb90068f838da1e44abd032f275a8909b563e31
MatsueSS <newmailnoways199@gmail.com>
2025-02-08 06:10:26 +0300

    Added comments in Hasher.h

--- include/Hasher.h
+++ include/Hasher.h
@@ -1,12 +1,15 @@
 #ifndef _HASHER_H_
 #define _HASHER_H_
 
+//Need for hash password, which clients give for server for authentification
+
 #include <sodium.h>
 #include <string>
 
 class Hasher{
 public:
     static std::string make_hash(const std::string&);
+
     static bool verify_password(const std::string&, const std::string&);
 };
 
