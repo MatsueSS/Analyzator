40771a3cbe46ffe69bc05c183903812b837e80cc
MatsueSS <newmailnoways199@gmail.com>
2025-02-08 10:36:44 +0300

    In Auth.h edited unique_ptr on unique_ptr&. Add func-wrapper.

--- include/Auth.h
+++ include/Auth.h
@@ -10,19 +10,21 @@
 
 class Auth : public Handle {
 public:
-    Auth(std::unique_ptr<PostgresDB>);
+    Auth(const std::unique_ptr<PostgresDB>&);
 
     virtual int handle(const Client&) override;
 
 private:
-    std::unique_ptr<PostgresDB> conn;
+    const std::unique_ptr<PostgresDB>& conn;
     enum action { auth = 0, reg = 1, noone = -1};
 
     action reg_or_auth(const std::string&, std::string&, std::string&);
-    bool make_auth(const std::string&, const std::string&);
-    void make_reg(const std::string&, const std::string&, const std::string&);
+    int make_auth(const std::string&, const std::string&);
+    int make_reg(const std::string&, const std::string&, const std::string&);
     void error(int);
     void bad_client(int);
+    void hello(int);
+    void bad_reg(int);
 
 };
 
