627ee21c470ce4f00c2238758c51f80a29cf57bc
MatsueSS <newmailnoways199@gmail.com>
2025-02-09 08:52:22 +0300

    Create new header - Info_value.h

---
+++ include/Info_value.h
@@ -0,0 +1,21 @@
+#ifndef _INFO_VALUE_H_
+#define _INFO_VALUE_H_
+
+#include <string>
+
+class Info_value{
+public:
+    Info_value(const std::string&, const std::string&);
+
+    std::string from_time_t() const;
+
+    std::string get_time_t() const;
+    std::string get_addr() const;
+
+private:
+    std::string ctime_str;
+    std::string addr;
+
+};
+
+#endif //_INFO_VALUE_H_
