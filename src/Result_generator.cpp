dd37822ba589631fd42ec619181438de16f4397d
MatsueSS <newmailnoways199@gmail.com>
2025-02-10 07:36:24 +0300

    Added definition Result_generator.h - Result_generator.cpp.

---
+++ src/Result_generator.cpp
@@ -0,0 +1,21 @@
+#include "Result_generator.h"
+
+#include <chrono>
+#include <fstream>
+
+Result_generator::Result_generator(std::shared_ptr<Compressor> ptr_com, std::shared_ptr<CSV_reader> ptr_csv)
+    : compressor(ptr_com), translator(ptr_csv) {}
+
+void Result_generator::generate_report() const
+{
+    time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::string time = ctime(&t);
+    time = std::string(time, 0, time.size()-1);
+    std::ofstream file("../res/" + time);
+    file << "Daily result on " << time << '\n';
+    const std::unordered_map<Code_value, int>& preresult = compressor->get_result();
+    for(auto it = preresult.begin(); it != preresult.end(); it++){
+        file << translator->get_definition(it->first) << ": " << it->second << '\n';
+    }
+    file.close();
+}
