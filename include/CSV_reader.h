f09653103f5b396e632227beb6aa4a99ce2fbeb7
MatsueSS <newmailnoways199@gmail.com>
2025-02-10 06:15:13 +0300

    Create a new definition file - CSV_reader.h.

---
+++ include/CSV_reader.h
@@ -0,0 +1,24 @@
+#ifndef _CSV_READER_H_
+#define _CSV_READER_H_
+
+#include "Code_value.h"
+
+#include <string>
+#include <unordered_map>
+
+class CSV_reader{
+public:
+    CSV_reader(const std::string&);
+    CSV_reader(const CSV_reader&) = delete;
+    CSV_reader(CSV_reader&&) noexcept;
+
+    CSV_reader& operator=(const CSV_reader&) = delete;
+    CSV_reader& operator=(CSV_reader&&) noexcept;
+
+    std::string get_definition(const Code_value&) const;
+
+private:
+    std::unordered_map<Code_value, std::string> definition_codes;
+};
+
+#endif //_CSV_READER_H_
