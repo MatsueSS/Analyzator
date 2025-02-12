ad4bb794c69a218e7eb00f8bd324b88c08e5fd28
MatsueSS <newmailnoways199@gmail.com>
2025-02-12 04:57:56 +0300

    Create a new file for start programm.

---
+++ include/Analyzer.h
@@ -0,0 +1,14 @@
+#ifndef _ANALYZER_H_
+#define _ANALYZER_H_
+
+class Analyzer{
+public:
+    Analyzer();
+
+    void start_process();
+
+private:
+    void time_check();
+};
+
+#endif //_ANALYZER_H_
