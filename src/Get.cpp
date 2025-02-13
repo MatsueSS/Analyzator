a205945ef27dc77552300298d0633fd5bcf0a092
MatsueSS <newmailnoways199@gmail.com>
2025-02-13 05:32:07 +0300

    Added definition Get.h - Get.cpp. In this class i use write - it's in future need correct, but now it's need correct all logic.

---
+++ src/Get.cpp
@@ -0,0 +1,20 @@
+#include "Get.h"
+
+#include <unistd.h>
+
+#define SUCCESS 1
+
+int Get::handle(const Client& obj, std::unique_ptr<PostgresDB>& db)
+{
+    if(!read_fd(obj.sockfd))
+        return disconnect;
+    
+    std::string query = "SELECT * FROM data WHERE resourse_name = $1 AND id = $2";
+    std::vector<std::vector<std::string>> result = db->fetch(query, {buf, std::to_string(id)});
+    if(result.empty())
+        return not_exist;
+
+    write(obj.sockfd, result[0][2].c_str(), result[0][2].size());
+
+    return SUCCESS;
+}
