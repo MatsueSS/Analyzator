6c60f4f85170b8b0c4212569accb2f8e4e0eaf70
MatsueSS <newmailnoways199@gmail.com>
2025-02-13 08:46:51 +0300

    Added definition Edit.h - Edit.cpp.

---
+++ src/Edit.cpp
@@ -0,0 +1,12 @@
+#include "Edit.h"
+
+int Edit::handle(const Client& obj, std::unique_ptr<PostgresDB>& db)
+{
+    if(!read_fd(obj.sockfd))
+        return disconnect;
+
+    std::string query = "UPDATE users SET password = $1 WHERE id = $2;";
+    db->execute(query, {buf, std::to_string(id)});
+    make_transaction(edit, id, buf, sock_ntop((sockaddr*)&obj.cliaddr), db);
+    return SUCCESS;
+}
