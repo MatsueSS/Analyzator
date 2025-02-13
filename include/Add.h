a698831c9f09026308a12f6fe27d8a3a002db7f0
MatsueSS <newmailnoways199@gmail.com>
2025-02-13 07:16:21 +0300

    In Add.h push new func - is_not_exist - need for test data on unique.

--- include/Add.h
+++ include/Add.h
@@ -6,6 +6,9 @@
 class Add : public Command_checker{
 public:
     virtual int handle(const Client&, std::unique_ptr<PostgresDB>&) override;
+
+private:
+    bool is_not_exist(const std::string&, int, std::unique_ptr<PostgresDB>&);
 };
 
 #endif //_ADD_H_
