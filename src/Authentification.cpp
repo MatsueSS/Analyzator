4ab9fbd1076c0bf29767c7ff35290b90815c09ad
MatsueSS <newmailnoways199@gmail.com>
2025-02-16 11:57:52 +0300

    In Handlers added check error with connect data base and reconnect. Edited - Add.cpp, Authentification.cpp, Del.cpp, Edit.cpp, Get.cpp, Registration.cpp.

--- src/Authentification.cpp
+++ src/Authentification.cpp
@@ -16,6 +16,14 @@ int Authentification::handle(const Client& obj, std::unique_ptr<PostgresDB>& db)
     if(name.empty() || pass.empty())
         return empty_values;
 
+    if(!db->is_connect()){
+        Log::make_note("2001");
+        if(!db->connect()){
+            Log::make_note("202");
+            return disconnect;
+        }
+    }
+
     std::string query = "SELECT * FROM users WHERE name_user = $1;";
     std::vector<std::vector<std::string>> result = db->fetch(query, {name});
 
--- src/Get.cpp
+++ src/Get.cpp
@@ -1,4 +1,5 @@
 #include "Get.h"
+#include "Log.h"
 
 #include <unistd.h>
 
@@ -6,6 +7,14 @@ int Get::handle(const Client& obj, std::unique_ptr<PostgresDB>& db)
 {
     if(!read_fd(obj.sockfd))
         return disconnect;
+
+    if(!db->is_connect()){
+        Log::make_note("2001");
+        if(!db->connect()){
+            Log::make_note("202");
+            return disconnect;
+        }
+    }
     
     std::string query = "SELECT * FROM data WHERE resourse_name = $1 AND id = $2";
     std::vector<std::vector<std::string>> result = db->fetch(query, {buf, std::to_string(obj.id)});
